57770fcfc875b5f4c1c30ec43a2fcefabd95697e
Arjan <44190435+vingerha@users.noreply.github.com>
2025-03-25 08:56:32 +0100

    Update bridge.py

--- src/linkplay/bridge.py
+++ src/linkplay/bridge.py
@@ -125,13 +125,12 @@ class LinkPlayPlayer:
     bridge: LinkPlayBridge
     properties: dict[PlayerAttribute, str]
     custom_properties: dict[PlayerAttribute, str]
-    metainfo: dict[MetaInfoMetaData,str]
+    metainfo: dict[str,str]
 
     def __init__(self, bridge: LinkPlayBridge):
         self.bridge = bridge
         self.properties = dict.fromkeys(PlayerAttribute.__members__.values(), "")
         self.custom_properties = dict.fromkeys(PlayerAttribute.__members__.values(), "")
-        self.metainfo = dict.fromkeys(MetaInfoMetaData.__members__.values(), "")
 
     def to_dict(self):
         """Return the state of the LinkPlayPlayer."""
@@ -145,7 +144,7 @@ class LinkPlayPlayer:
 
         self.properties = fixup_player_properties(properties)
         if self.bridge.device.manufacturer == MANUFACTURER_WIIM:
-            metainfo: dict[MetaInfoMetaData, str] = await self.bridge.json_request(
+            metainfo: dict[str, str] = await self.bridge.json_request(
                 LinkPlayCommand.META_INFO) # type: ignore[assignment]
             self.metainfo = metainfo
         else:
