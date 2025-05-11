4d998e7c6d8ff7e59564bdb0b1383f6df825124a
Simon Lamon <32477463+silamon@users.noreply.github.com>
2025-05-11 09:11:45 +0000

    Formatting

--- src/linkplay/exceptions.py
+++ src/linkplay/exceptions.py
@@ -7,6 +7,6 @@ class LinkPlayRequestException(LinkPlayException):
 
 
 class LinkPlayInvalidDataException(LinkPlayException):
-    def __init__(self, message: str = "Invalid data received", data: str = None):
+    def __init__(self, message: str = "Invalid data received", data: str | None = None):
         super().__init__(message)
         self.data = data
