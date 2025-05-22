84f021e815628c3df0e026067a5b810771d092c3
Arjan <44190435+vingerha@users.noreply.github.com>
2025-05-22 10:04:12 +0200

    Update bridge.py

--- src/linkplay/bridge.py
+++ src/linkplay/bridge.py
@@ -296,9 +296,10 @@ class LinkPlayPlayer:
         """Set the audio hardware output."""
         await self.bridge.request(LinkPlayCommand.AUDIO_OUTPUT_HW_MODE_SET.format(mode))
 
+    @property
     def get_audio_output_hw_mode(self) -> None:
         """Get the audio hardware output."""
-        await self.bridge.json_request(LinkPlayCommand.AUDIO_OUTPUT_HW_MODE)
+        return self.bridge.json_request(LinkPlayCommand.AUDIO_OUTPUT_HW_MODE)
 
     @property
     def muted(self) -> bool:
